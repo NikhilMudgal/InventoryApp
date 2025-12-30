d3903701ae78a412b66da451be77dd79420c5161
Nikhil Mudgal <nikhilm2097@gmail.com>
2025-12-30 15:01:57 +0530

    Created a dummy array and fetched the products

--- main.py
+++ main.py
@@ -2,8 +2,22 @@
 #     app = FastAPI()
 #     return app
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
+@app.get("/")
 def greet():
     return "Welcome"
+
+
+
+products = [
+    Product(1, "Mobile", "Affordable Mobile", 100, 40),
+    Product(2, "Laptop", "Gaming Laptop", 300, 15),
+    Product(3, "Speakers", "Wireless Speakers", 200, 30)
+]
+
+@app.get("/products")
+def get_products():
+    return products
