713f364bdde6b7203730c832c4e90dd1a5067915
Nikhil Mudgal <nikhilm2097@gmail.com>
2025-12-30 15:11:11 +0530

    Used pydantic in model file for data validation

--- main.py
+++ main.py
@@ -13,9 +13,16 @@ def greet():
 
 
 products = [
-    Product(1, "Mobile", "Affordable Mobile", 100, 40),
-    Product(2, "Laptop", "Gaming Laptop", 300, 15),
-    Product(3, "Speakers", "Wireless Speakers", 200, 30)
+    Product(id=1, name="Mobile", description="Affordable Mobile", price=100, quantity=40),
+    Product(id=2, name="Laptop", description="Gaming Laptop", price=300, quantity=15),
+    Product(id=3, name="Speakers", description="Wireless Speakers", price=200, quantity=30),
+    Product(id=4, name="Headphones", description="Wireless Headphones", price=150, quantity=20),
+    Product(id=5, name="Tablet", description="Tablet", price=250, quantity=10),
+    Product(id=6, name="Smartwatch", description="Smartwatch", price=300, quantity=15),
+    Product(id=7, name="Keyboard", description="Wireless Keyboard", price=100, quantity=20),
+    Product(id=8, name="Mouse", description="Wireless Mouse", price=50, quantity=30),
+    Product(id=9, name="Monitor", description="Monitor", price=400, quantity=10),
+    Product(id=10, name="Printer", description="Printer", price=350, quantity=15)
 ]
 
 @app.get("/products")
